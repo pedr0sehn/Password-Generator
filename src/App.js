ee92f3e816bc109322f3557c51ebc64ee2692983
Pedro Sehn <pedroarthursehn@gmail.com>
2023-10-09 18:52:15 -0300

    changed characters to symbols

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
   const [markLetter, setMarkLetter] = useState(true)
   const [markWord, setMarkWord] = useState(false)
   const [markNumber, setMarkNumber] = useState(false)
-  const [markSymbols, setMarkSymbols] = useState(false)
+  const [markSymbol, setMarkSymbol] = useState(false)
   const [passwordLength, setPasswordLength] = useState('16')
 
   return (
     <div className="App">
       <h1>Password Generator</h1>
-      <PasswordInfo markLetter={markLetter} setMarkLetter={setMarkLetter} markWord={markWord} setMarkWord={setMarkWord} markNumber={markNumber} setMarkNumber={setMarkNumber} markCharacter={markSymbols} setMarkCharacter={setMarkSymbols} />
+      <PasswordInfo markLetter={markLetter} setMarkLetter={setMarkLetter} markWord={markWord} setMarkWord={setMarkWord} markNumber={markNumber} setMarkNumber={setMarkNumber} markSymbol={markSymbol} setMarkSymbols={setMarkSymbol} />
       <CountInput passwordLength={passwordLength} setPasswordLength={setPasswordLength} />
-      <GeneratePassword letters={markLetter} words={markWord} numbers={markNumber} characters={markCharacter} numberOfDigits={passwordLength} />
+      <GeneratePassword letters={markLetter} words={markWord} numbers={markNumber} symbols={markSymbol} numberOfDigits={passwordLength} />
       <Footer />
     </div>
   );
